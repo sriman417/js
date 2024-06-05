e106fcdfbf6c121fc84e93677c83ce9a6796357f
Nainala Srimannarayana <105102064+sriman417@users.noreply.github.com>
2024-06-05 20:23:25 +0530

    Create const.js

---
+++ const.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// Challenge 1: We want to log out the name "Reed", but it doesn't work. Can you fix it?
+const name;
+name = "Reed";
+console.log(name);
+
+// Challenge 2: We want 102 to be logged out. But we're getting an error instead. Try to fix it!
+const count = 100;
+count = 101;
+count = 102;
+console.log(count);
+
+//  Challenge 3: We want to log out the price (50), but it doesn't work. Can you fix it?
+let price;
+console.log(price);
+price = 50;
+
+
+// my answer 
+
+'use strict';
+const name = "Reed";
+console.log(name);
+let count = 100;
+count = 101;
+count = 102;
+console.log(count);
+let price;
+price = 50;
+console.log(price);
