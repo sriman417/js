ec033e0c5a14f4c737a7c22c34426d5ae752b9f1
Nainala Srimannarayana <105102064+sriman417@users.noreply.github.com>
2024-06-05 21:17:21 +0530

    Create temp.js

---
+++ temp.js
@@ -0,0 +1,13 @@
+// Challenge: Help Darth Vader express himself to his son!
+const mySon = "Luke";
+const parentalStatus = "father";
+const message = "mySon, I am your parentalStatus";
+console.log(message);
+
+
+// my answer 
+
+const mySon = "Luke";
+const parentalStatus = "father";
+const message = '${mySon}, I am your ${parentalStatus}';
+console.log(message);
