8f6b114bcc0c85dd1f1b67acc9d836e268bc5031
Nainala Srimannarayana <105102064+sriman417@users.noreply.github.com>
2024-06-05 20:14:17 +0530

    Create var.js

---
+++ var.js
@@ -0,0 +1,20 @@
+// Fix the variable names below, and log them out to the console!
+
+var firstname = "Reed";
+
+var sur-name = "Barger";
+
+var 1000 = "one thousand";
+
+
+// Need help? Check out the hint.js file
+// this is the given question
+
+// my answer
+
+var firstname = "Reed";
+var surname = "Barger";
+var one1000 = "one thousand";
+console.log(firstname)
+console.log(surname)
+console.log(one1000)
