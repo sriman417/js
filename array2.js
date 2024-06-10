5347f2b41a1d3e0e469b7feafe9c7e2726c91b2b
Nainala Srimannarayana <105102064+sriman417@users.noreply.github.com>
2024-06-10 17:53:53 +0530

    Create array2.js

---
+++ array2.js
@@ -0,0 +1,30 @@
+// Challenge: 
+// Use reduce() to sum the weights of all the cars
+// Stretch goal: 
+// Use reduce to sum the weights of only the electric cars 
+
+const cars = [
+  { name: "Toyota", isElectric: false, weight: 1320 },
+  { name: "Ford", isElectric: false, weight: 1400 },
+  { name: "Volkswagen", isElectric: false, weight: 1370 },
+  { name: "Honda", isElectric: false, weight: 1375 },
+  { name: "Tesla", isElectric: true, weight: 1750 },
+  { name: "BMW", isElectric: true, weight: 1350 },  
+];
+
+
+// My answer 
+
+const cars = [
+  { name: "Toyota", isElectric: false, weight: 1320 },
+  { name: "Ford", isElectric: false, weight: 1400 },
+  { name: "Volkswagen", isElectric: false, weight: 1370 },
+  { name: "Honda", isElectric: false, weight: 1375 },
+  { name: "Tesla", isElectric: true, weight: 1750 },
+  { name: "BMW", isElectric: true, weight: 1350 },  
+];
+const Weight = cars.reduce((sum, car) => sum + car.weight, 0);
+console.log(Weight);
+const electricCars = cars.filter(car => car.isElectric);
+const ElectricWeight = electricCars.reduce((sum, car) => sum + car.weight, 0);
+console.log(ElectricWeight);
