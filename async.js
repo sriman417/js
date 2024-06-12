aae16a7436e1f715d40137d3e8d5fce8a0b7e95d
Nainala Srimannarayana <105102064+sriman417@users.noreply.github.com>
2024-06-12 19:57:31 +0530

    Create async.js

---
+++ async.js
@@ -0,0 +1,32 @@
+// Challenge: 
+// Rewrite the GET API call from the previous challenge using async-await
+
+
+// Challenge: 
+fetch('https://jsonplaceholder.typicode.com/users/3')
+  .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);  
+      }
+      
+      return response.json()
+  })
+  .then(person => console.log(`${person.name} works for ${person.company.name}`))
+  .catch(err => console.error(err))
+  
+// my answer 
+
+async function getUser() {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users/3');
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    const person = await response.json();
+    console.log(person);    
+  }
+    catch (error) {
+      console.log(error);
+  }
+}
+getUser()
